fdd6e4f119f276b915c9728c7e33a4becd73eea3
Venkatesh-Krishnan-R <157407663+Venkatesh-Krishnan-R@users.noreply.github.com>
2024-06-18 11:20:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "4d01b490-7099-4b7a-a2f7-337cee5177c7"
-  tenant_id       = "3fce1092-8f1c-40fb-a540-71f39fb36a43"
-  client_id       = "3d7a7a3b-d4cd-4285-9105-cb2a978bdc2b"
-  client_secret   = "vn_8Q~vS4rw4VoT-QmSshA_5Ai8Yy0MNOSZ7lbCi"
+  subscription_id =  #
+  tenant_id       =  #
+  client_id       =  #
+  client_secret   =  #
   features {}
 }
